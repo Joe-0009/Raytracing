f6b285dd5ab096f8207ec26b220c6b7e6ce4ccea
Joe-0009 <youssefrachidi1994@gmail.com>
2025-06-23 16:32:04 +0100

    remove comm

--- minirt/src/utils/vector_ops.c
+++ minirt/src/utils/vector_ops.c
@@ -12,56 +12,47 @@ t_vec3	vec3_create(double x, double y, double z)
 	return (v);
 }
 
-// Add two vectors
 t_vec3	vec3_add(t_vec3 v1, t_vec3 v2)
 {
 	return (vec3_create(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z));
 }
 
-// Subtract two vectors
 t_vec3	vec3_sub(t_vec3 v1, t_vec3 v2)
 {
 	return (vec3_create(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z));
 }
 
-// Multiply vector by scalar
 t_vec3	vec3_mult(t_vec3 v, double t)
 {
 	return (vec3_create(v.x * t, v.y * t, v.z * t));
 }
 
-// Divide vector by scalar
 t_vec3	vec3_div(t_vec3 v, double t)
 {
 	return (vec3_create(v.x / t, v.y / t, v.z / t));
 }
 
-// Cross product
 t_vec3	vec3_cross(t_vec3 v1, t_vec3 v2)
 {
 	return (vec3_create(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z,
 			v1.x * v2.y - v1.y * v2.x));
 }
 
-// Dot product
 double	vec3_dot(t_vec3 v1, t_vec3 v2)
 {
 	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
 }
 
-// Length squared
 double	vec3_length_squared(t_vec3 v)
 {
 	return (vec3_dot(v, v));
 }
 
-// Length
 double	vec3_length(t_vec3 v)
 {
 	return (sqrt(vec3_length_squared(v)));
 }
 
-// Normalize
 t_vec3	vec3_normalize(t_vec3 v)
 {
 	double	len;
@@ -72,13 +63,11 @@ t_vec3	vec3_normalize(t_vec3 v)
 	return (vec3_div(v, len));
 }
 
-// Reflect vector v around normal n
 t_vec3	reflect(t_vec3 v, t_vec3 n)
 {
 	return (vec3_sub(v, vec3_mult(n, 2 * vec3_dot(v, n))));
 }
 
-// Rotate vector v around axis by angle (radians)
 t_vec3	vec3_rotate_around_axis(t_vec3 v, t_vec3 axis, double angle)
 {
 	t_vec3	u;
