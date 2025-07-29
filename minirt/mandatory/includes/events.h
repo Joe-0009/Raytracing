e87f8bfb4a80b033eff9d06af80e9778ee18f164
Joe-0009 <youssefrachidi1994@gmail.com>
2025-07-29 08:45:09 +0100

    change keys from mac to lunix

--- minirt/mandatory/includes/events.h
+++ minirt/mandatory/includes/events.h
@@ -3,35 +3,35 @@
 
 # include "minirt_app.h"
 
-/* Key codes for standard MLX */
-# define KEY_ESC 53
-# define KEY_W 13
-# define KEY_A 0
-# define KEY_S 1
-# define KEY_D 2
-# define KEY_Q 12
-# define KEY_E 14
-# define KEY_UP 126
-# define KEY_DOWN 125
-# define KEY_LEFT 123
-# define KEY_RIGHT 124
-# define KEY_PLUS 24
-# define KEY_MINUS 27
-# define KEY_R 15
-# define KEY_T 17
-# define KEY_F 3
-# define KEY_G 5
-# define KEY_I 34
-# define KEY_J 38
-# define KEY_K 40
-# define KEY_L 37
-# define KEY_SPACE 49
-# define KEY_P 35
-# define KEY_O 31
-# define KEY_U 32
-# define KEY_Y 16
-# define KEY_H 4
-# define KEY_N 45
+/* Key codes for Linux (X11) */
+# define KEY_ESC        65307
+# define KEY_W          119
+# define KEY_A          97
+# define KEY_S          115
+# define KEY_D          100
+# define KEY_Q          113
+# define KEY_E          101
+# define KEY_UP         65362
+# define KEY_DOWN       65364
+# define KEY_LEFT       65361
+# define KEY_RIGHT      65363
+# define KEY_PLUS       61
+# define KEY_MINUS      45
+# define KEY_R          114
+# define KEY_T          116
+# define KEY_F          102
+# define KEY_G          103
+# define KEY_I          105
+# define KEY_J          106
+# define KEY_K          107
+# define KEY_L          108
+# define KEY_SPACE      32
+# define KEY_P          112
+# define KEY_O          111
+# define KEY_U          117
+# define KEY_Y          121
+# define KEY_H          104
+# define KEY_N          110
 
 
 
